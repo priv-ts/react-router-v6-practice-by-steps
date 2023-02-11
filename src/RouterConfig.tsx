a78c114c4e164ec8e16f25439fa5e0bcb199ac2e
privts <priv.tomokisato@gmail.com>
2023-02-12 00:37:44 +0900

    ダイナミックルーティングを設定する
    - ページコンポーネント内で、ルーティングで渡すクエリパラメータをuseParamsで受け取る
    - RouterConfigのRouteでクエリパラメータで受け取るパスパラメータをセットしてコンポーネントに渡す
    - Linkコンポーネントでページ遷移時に渡すパラメータをセットする

--- src/RouterConfig.tsx
+++ src/RouterConfig.tsx
@@ -9,6 +9,7 @@ import {
   SamplePage4,
   SamplePage4Child1,
   SamplePage4Child2,
+  SamplePage4Child3,
 } from "./components/SamplePage4";
 
 const RouterConfig: React.VFC = () => {
@@ -31,6 +32,7 @@ const RouterConfig: React.VFC = () => {
             <Route index />
             <Route path="child1" element={<SamplePage4Child1 />} />
             <Route path="child2" element={<SamplePage4Child2 />} />
+            <Route path=":childId" element={<SamplePage4Child3 />} />
           </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
--- src/components/SamplePage4.tsx
+++ src/components/SamplePage4.tsx
@@ -1,18 +1,27 @@
 import React from "react";
-import { Outlet, Link, useNavigate } from "react-router-dom";
+import {
+  Outlet,
+  Link,
+  useParams,
+  useNavigate,
+  NavigateFunction,
+} from "react-router-dom";
 
 export const SamplePage4: React.VFC = () => {
-  const navigate = useNavigate();
+  const navigate: NavigateFunction = useNavigate();
 
   return (
     <>
-      <h3>Sample Page 3</h3>
+      <h3>Sample Page 4</h3>
       <ul>
         <li>
           <Link to="child1">Show Child 1</Link>
         </li>
         <li>
           <Link to="child2">Show Child 2</Link>
+        </li>
+        <li>
+          <Link to="123">Show Child 3</Link>
         </li>
       </ul>
       <Outlet />
@@ -27,4 +36,19 @@ export const SamplePage4Child1: React.VFC = () => {
 
 export const SamplePage4Child2: React.VFC = () => {
   return <h3>Sample Page 4 Child 2</h3>;
+};
+
+export const SamplePage4Child3: React.VFC = () => {
+  type Param = {
+    childId?: string;
+  };
+
+  const params: Param = useParams<Param>();
+
+  return (
+    <>
+      <h3>Sample Page 4 Child 3</h3>
+      <p>{`childId=${params?.childId}`}</p>
+    </>
+  );
 };
